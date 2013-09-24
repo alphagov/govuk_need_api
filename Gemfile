76f5596264eaa70f3a1d9d00c011fc22c1ea2e3c
Jordan Hatch <jordan@jordanh.net>
2013-09-24 12:39:22 +0100

    Set up Test::Unit with a default test for CI

--- Gemfile
+++ Gemfile
@@ -10,10 +10,16 @@ else
   gem 'gds-sso', '3.1.0'
 end
 
+group :test do
+  gem 'capybara', '2.1.0'
+  gem 'database_cleaner', '1.1.1', require: false
+  gem 'factory_girl_rails', '4.2.1'
+  gem 'shoulda-context', '1.1.5'
+end
+
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
   gem 'uglifier', '>= 1.0.3'
 end
 
---
+++ test/test_helper.rb
@@ -0,0 +1,30 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
+
+class ActiveSupport::TestCase
+  teardown do
+    DatabaseCleaner.clean
+  end
+
+  def stub_user
+    @stub_user ||= FactoryGirl.create(:user, :name => 'Stub User')
+  end
+
+  def login_as_stub_user
+    login_as stub_user
+  end
+
+  def login_as(user)
+    request.env['warden'] = stub(
+      :authenticate! => true,
+      :authenticated? => true,
+      :user => user
+    )
+  end
+end
