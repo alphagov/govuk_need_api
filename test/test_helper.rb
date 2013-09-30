b820b43c7b17141739573ab3fb20cd1bd7f2a4d0
Jordan Hatch <jordan@jordanh.net>
2013-09-30 17:31:47 +0100

    Add mocha gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'database_cleaner'
+require 'mocha'
 
 require 'simplecov'
 require 'simplecov-rcov'
