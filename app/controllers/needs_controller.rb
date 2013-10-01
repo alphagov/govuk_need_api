87ade77c21e8c47be2424a98a9331169486aac42
Jordan Hatch <jordan@jordanh.net>
2013-10-01 14:09:30 +0100

    Return an error response given invalid attributes

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -5,6 +5,8 @@ class NeedsController < ApplicationController
     if @need.save
       render json: NeedPresenter.new(@need, view_context).as_json(status: :created),
              status: :created
+    else
+      error 422, message: :invalid_attributes, errors: @need.errors.full_messages
     end
   end
 end
--- test/functional/needs_controller_test.rb
+++ test/functional/needs_controller_test.rb
@@ -48,6 +48,31 @@ class NeedsControllerTest < ActionController::TestCase
         assert_equal "contact them about a local enquiry", body["benefit"]
       end
     end
+
+    context "given invalid attributes" do
+      setup do
+        @need = {
+          role: "user",
+          goal: "",
+          benefit: "contact them about a local enquiry"
+        }
+      end
+
+      should "return a 422 status code" do
+        post :create, need: @need
+
+        assert_equal 422, response.status
+      end
+
+      should "return model errors in the json response" do
+        post :create, need: @need
+
+        body = JSON.parse(response.body)
+        assert_equal "invalid_attributes", body["_response_info"]["status"]
+        assert_equal 1, body["errors"].length
+        assert_equal "Goal can't be blank", body["errors"].first
+      end
+    end
   end
 
 end
