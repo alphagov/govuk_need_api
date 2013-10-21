cdebdf0ad17706baa1cead0f87b2d6fbac0d85a2
Stuart Gale <bishboria@gmail.com>
2013-10-21 09:41:00 +0000

    Filter by organisation

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,6 +1,12 @@
 class NeedsController < ApplicationController
   def index
-    @needs = Need.all
+    query = filtered_params
+    @needs = if query.empty?
+               Need.all
+             else
+               orgs = *query["organisation_id"]
+               Need.where(:organisation_ids.all => orgs)
+             end
 
     set_expiry 0
     render json: NeedResultSetPresenter.new(@needs).as_json
--- test/integration/retrieving_needs_test.rb
+++ test/integration/retrieving_needs_test.rb
@@ -7,23 +7,24 @@ class RetrievingNeedsTest < ActionDispatch::IntegrationTest
 
     FactoryGirl.create(:organisation, name: "Department for Work and Pensions", slug: "department-for-work-and-pensions")
     FactoryGirl.create(:organisation, name: "HM Treasury", slug: "hm-treasury")
+
+    FactoryGirl.create(:need, role: "car owner",
+                       goal: "renew my car tax",
+                       benefit: "I can drive my car for another year",
+                       organisation_ids: ["hm-treasury"])
+    FactoryGirl.create(:need, role: "student",
+                       goal: "apply for student finance",
+                       benefit: "I can get the money I need to go to university",
+                       organisation_ids: ["department-for-work-and-pensions"])
+    FactoryGirl.create(:need, role: "jobseeker",
+                       goal: "search for jobs",
+                       benefit: "I can get into work",
+                       organisation_ids: ["department-for-work-and-pensions", "hm-treasury"])
+
   end
 
   context "listing all needs" do
     should "return basic information about all the needs" do
-      FactoryGirl.create(:need, role: "car owner",
-                                goal: "renew my car tax",
-                                benefit: "I can drive my car for another year",
-                                organisation_ids: ["hm-treasury"])
-      FactoryGirl.create(:need, role: "student",
-                                goal: "apply for student finance",
-                                benefit: "I can get the money I need to go to university",
-                                organisation_ids: ["department-for-work-and-pensions"])
-      FactoryGirl.create(:need, role: "jobseeker",
-                                goal: "search for jobs",
-                                benefit: "I can get into work",
-                                organisation_ids: ["department-for-work-and-pensions", "hm-treasury"])
-
       get "/needs"
       assert_equal 200, last_response.status
 
@@ -39,6 +40,20 @@ class RetrievingNeedsTest < ActionDispatch::IntegrationTest
 
       assert_equal "hm-treasury", body["results"][0]["organisations"][0]["id"]
       assert_equal "HM Treasury", body["results"][0]["organisations"][0]["name"]
+    end
+  end
+
+  context "filtering needs by organisation" do
+    should "return the needs required by that organisation" do
+      get "/needs?organisation_id=hm-treasury"
+      body = JSON.parse(last_response.body)
+
+      assert_equal 200, last_response.status
+      assert_equal "ok", body["_response_info"]["status"]
+      assert_equal 2, body["results"].size
+      assert_equal ["car owner", "jobseeker"], body["results"].map{|n| n["role"] }
+      assert_equal ["renew my car tax", "search for jobs"], body["results"].map{|n| n["goal"] }
+      assert_equal ["I can drive my car for another year", "I can get into work"], body["results"].map{|n| n["benefit"] }
     end
   end
 end
