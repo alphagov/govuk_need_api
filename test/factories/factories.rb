5ec25743c9d8fc0c5e4284239125129038abd81f
Jordan Hatch <jordan@jordanh.net>
2013-10-21 17:08:53 +0100

    Add some more attributes to the Need factory

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -14,5 +14,18 @@ FactoryGirl.define do
     role "user"
     goal "pay my council tax"
     benefit "I don't receive a fine"
+
+    justifications [ "legislation", "other" ]
+    impact "Has serious consequences for the day-to-day lives of your users"
+    met_when ["user can pay their council tax"]
+
+    monthly_user_contacts 300
+    monthly_site_views 100000
+    monthly_need_views 20000
+    monthly_searches 6000
+
+    currently_met false
+    other_evidence "This is important"
+    legislation "Council Tax Act 1994"
   end
 end
