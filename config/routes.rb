0f83ee04d53b24cf9a061f3303f91a25d00d106b
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-10-23 13:02:42 +0000

    Remove the HTML form actions from our routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 GovukNeedApi::Application.routes.draw do
   resources :organisations, :only => :index
-  resources :needs
+  resources :needs, :except => [:new, :edit]
 
   get "/healthcheck" => Proc.new { [200, {"Content-type" => "text/plain"}, ["OK"]] }
 
