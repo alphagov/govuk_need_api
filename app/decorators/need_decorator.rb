65879b91721bac66ccd1048a8dfee13ce311add9
Jordan Hatch <jordan@jordanh.net>
2013-10-28 17:54:01 +0000

    Add a comment to the NeedDecorator revisions_with_changes method

--- app/decorators/need_decorator.rb
+++ app/decorators/need_decorator.rb
@@ -2,6 +2,10 @@ require 'delegate'
 
 class NeedDecorator < SimpleDelegator
 
+  # Return all the revisions for the need as array pairs. Each pair contains a revision,
+  # decorated with the RevisionDecorator, along with the previous revision. The previous
+  # revision can be nil - for example if this is the first revision.
+  #
   def revisions_with_changes
     (need.revisions + [nil]).each_cons(2).map {|action, previous|
       [ RevisionDecorator.new(action), previous ]
