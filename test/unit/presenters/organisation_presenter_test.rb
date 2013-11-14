b3d7456aea85d46c95e2f904728de71ea01fc4f5
Stuart Gale <bishboria@gmail.com>
2013-11-14 10:48:02 +0000

    Assert OrganisationPresenter returns correct values

--- test/unit/presenters/organisation_presenter_test.rb
+++ test/unit/presenters/organisation_presenter_test.rb
@@ -6,7 +6,11 @@ class OrganisationPresenterTest < ActiveSupport::TestCase
     @organisation = OpenStruct.new(
       id: "ministry-of-joy",
       name: "Ministry of Joy",
-      slug: "ministry-of-joy"
+      slug: "ministry-of-joy",
+      govuk_status: "live",
+      abbreviation: "MOJ",
+      parent_ids: ["ministry-of-amusement"],
+      child_ids: ["ministry-of-elation","ministry-of-revelry"]
     )
     @presenter = OrganisationPresenter.new(@organisation)
   end
@@ -18,6 +22,10 @@ class OrganisationPresenterTest < ActiveSupport::TestCase
 
     assert_equal "ministry-of-joy", response[:id]
     assert_equal "Ministry of Joy", response[:name]
+    assert_equal "live", response[:govuk_status]
+    assert_equal "MOJ", response[:abbreviation]
+    assert_equal ["ministry-of-amusement"], response[:parent_ids]
+    assert_equal ["ministry-of-elation","ministry-of-revelry"], response[:child_ids]
   end
 
   should "return the basic attributes" do
@@ -25,5 +33,9 @@ class OrganisationPresenterTest < ActiveSupport::TestCase
 
     assert_equal "ministry-of-joy", response[:id]
     assert_equal "Ministry of Joy", response[:name]
+    assert_equal "live", response[:govuk_status]
+    assert_equal "MOJ", response[:abbreviation]
+    assert_equal ["ministry-of-amusement"], response[:parent_ids]
+    assert_equal ["ministry-of-elation","ministry-of-revelry"], response[:child_ids]
   end
 end
