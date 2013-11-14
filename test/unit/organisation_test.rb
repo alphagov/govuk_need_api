2bdce84dcc1b3ffc68e5a3850ff255deb817b356
Stuart Gale <bishboria@gmail.com>
2013-11-14 08:01:53 +0000

    Add new fields to organisation model

--- test/unit/organisation_test.rb
+++ test/unit/organisation_test.rb
@@ -6,7 +6,12 @@ class OrganisationTest < ActiveSupport::TestCase
     setup do
       @atts = {
         name: "Ministry of Magic",
-        slug: "ministry-of-magic"
+        slug: "ministry-of-magic",
+        abbreviation: "MOM",
+        govuk_status: "live",
+        parent_ids: ["ministry-of-sleight-of-hand"],
+        child_ids: ["ministry-for-creationism",
+                   "ministry-for-astrology"]
       }
     end
 
@@ -21,6 +26,10 @@ class OrganisationTest < ActiveSupport::TestCase
       assert_equal "Ministry of Magic", organisation.name
       assert_equal "ministry-of-magic", organisation.slug
       assert_equal "ministry-of-magic", organisation._id
+      assert_equal "MOM", organisation.abbreviation
+      assert_equal "live", organisation.govuk_status
+      assert_equal ["ministry-of-sleight-of-hand"], organisation.parent_ids
+      assert_equal ["ministry-for-creationism","ministry-for-astrology"], organisation.child_ids
     end
 
     should "not be valid without a name" do
