86a8fdbc4f82025f14582ae6170deb6f6af99296
Stuart Gale <bishboria@gmail.com>
2013-11-14 10:49:22 +0000

    No need to test OrganisationController returns specific values

--- test/functional/organisations_controller_test.rb
+++ test/functional/organisations_controller_test.rb
@@ -8,11 +8,9 @@ class OrganisationsControllerTest < ActionController::TestCase
 
   context "GET index" do
     setup do
-      @organisations = [
-        FactoryGirl.create(:organisation, name: "Department for Transport", slug: "department-for-transport"),
-        FactoryGirl.create(:organisation, name: "Home Office", slug: "home-office"),
-        FactoryGirl.create(:organisation, name: "Ministry of Justice", slug: "ministry-of-justice")
-      ]
+      FactoryGirl.create(:organisation, name: "Department for Transport", slug: "department-for-transport")
+      FactoryGirl.create(:organisation, name: "Home Office", slug: "home-office")
+      FactoryGirl.create(:organisation, name: "Ministry of Justice", slug: "ministry-of-justice")
     end
 
     should "return a successful response" do
@@ -22,22 +20,16 @@ class OrganisationsControllerTest < ActionController::TestCase
 
       body = JSON.parse(response.body)
       assert_equal "ok", body["_response_info"]["status"]
+      assert_equal 3, body["organisations"].size
     end
 
-    should "return a list of all organisations" do
+    should "present the organisations using as_json" do
+      result_set_presenter = OpenStruct.new(:as_json => "foo")
+      OrganisationResultSetPresenter.expects(:new).returns(result_set_presenter)
+
+      result_set_presenter.expects(:as_json)
+
       get :index
-
-      body = JSON.parse(response.body)
-      assert_equal 3, body["organisations"].size
-
-      assert_equal "department-for-transport", body["organisations"][0]["id"]
-      assert_equal "Department for Transport", body["organisations"][0]["name"]
-
-      assert_equal "home-office", body["organisations"][1]["id"]
-      assert_equal "Home Office", body["organisations"][1]["name"]
-
-      assert_equal "ministry-of-justice", body["organisations"][2]["id"]
-      assert_equal "Ministry of Justice", body["organisations"][2]["name"]
     end
   end
 
