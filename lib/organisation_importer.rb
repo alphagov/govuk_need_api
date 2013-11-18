0f1b11d1c299b3ef2b4f174839d61ffab0b42f6b
Stuart Gale <bishboria@gmail.com>
2013-11-18 10:56:42 +0000

    Improve variable names in OrganisationImporter

--- lib/organisation_importer.rb
+++ lib/organisation_importer.rb
@@ -4,31 +4,31 @@ class OrganisationImporter
   def run
     api = GdsApi::Organisations.new(Plek.current.find('whitehall-admin'))
     api.organisations.with_subsequent_pages.to_a.each do |org|
-      create_organisation(org)
+      create_or_update_organisation(org)
     end
   end
 
   private
 
-  def create_organisation(org)
-    child_ids = related_organisation_slugs(org.child_organisations)
-    parent_ids = related_organisation_slugs(org.parent_organisations)
+  def create_or_update_organisation(organisation_from_api)
+    child_ids = related_organisation_slugs(organisation_from_api.child_organisations)
+    parent_ids = related_organisation_slugs(organisation_from_api.parent_organisations)
 
-    org_hash = {
-      :name => org.title,
-      :slug => org.details.slug,
-      :abbreviation => org.details.abbreviation,
-      :govuk_status => org.details.govuk_status,
+    organisation_atts = {
+      :name => organisation_from_api.title,
+      :slug => organisation_from_api.details.slug,
+      :abbreviation => organisation_from_api.details.abbreviation,
+      :govuk_status => organisation_from_api.details.govuk_status,
       :parent_ids => parent_ids,
       :child_ids => child_ids
     }
 
-    o = Organisation.where(slug: org.details.slug).first
+    existing_organisation = Organisation.where(slug: organisation_from_api.details.slug).first
 
-    if o
-      o.update_attributes(org_hash)
+    if existing_organisation.present?
+      existing_organisation.update_attributes(organisation_atts)
     else
-      Organisation.new(org_hash).save
+      Organisation.new(organisation_atts).save
     end
   end
 
