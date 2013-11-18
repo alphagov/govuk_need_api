07ee689c325dfdc7a2dc3d7b3662b21abb40b5d9
Stuart Gale <bishboria@gmail.com>
2013-11-18 14:14:28 +0000

    Change unit tests to mock GdsApi::Organisations

--- test/unit/organisation_importer_test.rb
+++ test/unit/organisation_importer_test.rb
@@ -1,10 +1,7 @@
-require './test/integration_test_helper'
+require './test/test_helper'
 require './lib/organisation_importer'
-require 'gds_api/test_helpers/organisations'
 
 class OrganisationImporterTest < ActionDispatch::IntegrationTest
-  include GdsApi::TestHelpers::Organisations
-
   setup do
     FactoryGirl.create(:organisation, name: "HM Treasury", slug: "hm-treasury",
                        child_ids: [], parent_ids: [])
@@ -15,33 +12,36 @@ class OrganisationImporterTest < ActionDispatch::IntegrationTest
                        name: "Office of the Leader of the House of Commons",
                        slug: "office-of-the-leader-of-the-house-of-commons",
                        child_ids: [], parent_ids: [])
-    @hm_treasury = {
-      details: { slug: "hm-treasury" },
+    @hm_treasury = OpenStruct.new(
+      details: OpenStruct.new(slug: "hm-treasury"),
       title: "HM Treasury",
       child_organisations: [],
       parent_organisations: []
-    }
-    @dwp = {
-      details: { slug: "department-for-work-and-pensions" },
+    )
+    @dwp = OpenStruct.new(
+      details: OpenStruct.new(slug: "department-for-work-and-pensions"),
       title: "Department for Work & Pensions",
-      child_organisations: [{id:"equality-2025"}],
+      child_organisations: [OpenStruct.new(id:"equality-2025")],
       parent_organisations: []
-    }
-    @hoc = {
-      details: { slug: "the-office-of-the-leader-of-the-house-of-commons" },
+    )
+    @hoc = OpenStruct.new(
+      details: OpenStruct.new(slug: "the-office-of-the-leader-of-the-house-of-commons"),
       title: "Office of the Leader of the House of Commons",
-      child_organisations: [], parent_organisations: [{id:"cabinet-office"}]
-    }
+      child_organisations: [], parent_organisations: [OpenStruct.new(id:"cabinet-office")]
+    )
   end
 
   should "not update an organisation if no attributes have changed" do
-    req = request(@hm_treasury)
+    stub_subsequent_pages = stub
+    GdsApi::Organisations.any_instance.expects(:organisations)
+      .returns(stub_subsequent_pages)
+    stub_subsequent_pages.expects(:with_subsequent_pages)
+      .returns([@hm_treasury])
 
     OrganisationImporter.new.run
 
     org = Organisation.where(slug: "hm-treasury").first
 
-    assert_requested req
     assert_equal("HM Treasury", org.name)
     assert_equal("hm-treasury", org.slug)
     assert_equal([], org.child_ids)
@@ -49,20 +49,27 @@ class OrganisationImporterTest < ActionDispatch::IntegrationTest
   end
 
   should "update an organisation if an attribute has changed" do
-    req = request(@hm_treasury.merge(title: "HM Treasure Chest"))
+    stub_subsequent_pages = stub
+    GdsApi::Organisations.any_instance.expects(:organisations)
+      .returns(stub_subsequent_pages)
+    @hm_treasury.title = "HM Treasure Chest"
+    stub_subsequent_pages.expects(:with_subsequent_pages)
+      .returns([@hm_treasury])
 
     OrganisationImporter.new.run
 
-    assert_requested req
     assert_equal("HM Treasure Chest",
                  Organisation.where(slug: "hm-treasury").first.name)
   end
 
   should "update several organisations that have changed attributes" do
-    req = request(@dwp, @hoc)
-    OrganisationImporter.new.run
+    stub_subsequent_pages = stub
+    GdsApi::Organisations.any_instance.expects(:organisations)
+      .returns(stub_subsequent_pages)
+    stub_subsequent_pages.expects(:with_subsequent_pages)
+      .returns([@dwp, @hoc])
 
-    assert_requested req
+    OrganisationImporter.new.run
 
     dwp = Organisation.where(slug: "department-for-work-and-pensions").first
     hoc = Organisation.where(slug: "the-office-of-the-leader-of-the-house-of-commons").first
@@ -72,37 +79,31 @@ class OrganisationImporterTest < ActionDispatch::IntegrationTest
   end
 
   should "add a new organisation if not already present" do
-    req = request({
-      details: {
+    mod = OpenStruct.new(
+      details: OpenStruct.new(
         slug: "ministry-of-defence",
         govuk_status: "live"
-      },
+      ),
       title: "Ministry of Defence",
       abbreviation: "mod",
-      child_organisations: [{id:"advisory-committee-on-conscientious-objectors"}],
+      child_organisations: [OpenStruct.new(id:"advisory-committee-on-conscientious-objectors")],
       parent_organisations: []
-    })
+    )
+    stub_subsequent_pages = stub
+    GdsApi::Organisations.any_instance.expects(:organisations)
+      .returns(stub_subsequent_pages)
+    stub_subsequent_pages.expects(:with_subsequent_pages)
+      .returns([mod])
 
     assert_nil Organisation.where(slug: "ministry-of-defence").first
 
     OrganisationImporter.new.run
     mod = Organisation.where(slug: "ministry-of-defence").first
 
-    assert_requested req
     assert_equal("ministry-of-defence", mod.slug)
     assert_equal("Ministry of Defence", mod.name)
     assert_equal("live", mod.govuk_status)
     assert_equal(["advisory-committee-on-conscientious-objectors"], mod.child_ids)
     assert_equal([], mod.parent_ids)
-  end
-
-  def request(*org_details)
-    stub_request(:get, Plek.current.find('whitehall-admin')+'/api/organisations')
-      .to_return(status: 200,
-                 body: {
-                   _response_info: { status: "OK" },
-                   results: org_details
-                 }.to_json
-                )
   end
 end
