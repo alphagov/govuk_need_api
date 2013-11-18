7c6f07642da95cbf7b8f7a967310d95997c1d498
Stuart Gale <bishboria@gmail.com>
2013-11-18 12:33:45 +0000

    Add default values for optional organisation fields

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,10 +3,10 @@ class Organisation
 
   field :name, type: String
   field :slug, type: String
-  field :govuk_status, type: String
-  field :abbreviation, type: String
-  field :parent_ids, type: Array
-  field :child_ids, type: Array
+  field :govuk_status, type: String, default: ""
+  field :abbreviation, type: String, default: ""
+  field :parent_ids, type: Array, default: []
+  field :child_ids, type: Array, default: []
 
   key :slug
 
