44cb58478fd123cdceb12fcb7b30e3614bf147a2
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-11-27 17:21:29 +0000

    Document hackful reliance on initialiser ordering.
    It was already documented in the dependent initialiser, but there's no
    indiciation in the other one.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,3 @@
+# NOTE: the `search.rb` initializer relies on this file being loaded first,
+# which it currently manages because it comes first in the alphabet.
 GovukNeedApi.search_client = Elasticsearch::Client.new(host: "localhost:9200")
