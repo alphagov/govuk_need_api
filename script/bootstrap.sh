dba2a052295ff68d2aab8f5a4b10b9bd184e764e
Stuart Gale <bishboria@gmail.com>
2013-12-03 11:31:37 +0000

    Bootstrap script updates
    * Output what commands are running/complete
    * Kill script on first failure

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/sh -e
 
-bundle
+echo "Bundle start"
+bundle --quiet
+echo "Bundle complete"
+
 bundle exec rake db:setup
+echo "Database setup complete"
+
 GOVUK_APP_DOMAIN=production.alphagov.co.uk bundle exec rake organisations:import
+
 bundle exec rake search:reset
