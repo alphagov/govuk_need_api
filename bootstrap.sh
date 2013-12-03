04ce5febf274b2ccfc4cf2931ef4e2ae6d9a4b57
Stuart Gale <bishboria@gmail.com>
2013-12-03 11:19:16 +0000

    Update seeds to not recreate existing sigin user

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 bundle
-bundle exec rake db:seed
+bundle exec rake db:setup
 GOVUK_APP_DOMAIN=production.alphagov.co.uk bundle exec rake organisations:import
 bundle exec rake search:reset
