f64b4879e766d789f85e1118cdbb6c6a8d40c005
Stuart Gale <bishboria@gmail.com>
2014-02-27 13:57:38 +0000

    Add validations to note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,9 +9,21 @@ class Note
 
   default_scope order_by([:created_at, :desc])
 
+  validates_presence_of :text, :need_id, :author
+  validate :validate_need_id
+
   def save
-    need = Need.find(need_id)
-    self.revision = need.revisions.first.id
+    need = Need.where(need_id: need_id).first
+    self.revision = need.revisions.first.id if need
     super
+  end
+
+  private
+
+  def validate_need_id
+    need = Need.where(need_id: need_id).first
+    if need.nil?
+      errors.add(:need_id, "A note must have a valid need_id")
+    end
   end
 end
--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -15,7 +15,7 @@ class NoteTest < ActiveSupport::TestCase
   end
 
   should "be able to create a note" do
-    Need.expects(:find).returns(@need)
+    Need.expects(:where).twice.returns([@need])
 
     note = Note.new(@atts)
     note.save
@@ -24,5 +24,33 @@ class NoteTest < ActiveSupport::TestCase
     assert_equal "test", note.text
     assert_equal "Winston Smith-Churchill", note.author["name"]
     assert_equal @need.revisions.first.id.to_s, note.revision
+  end
+
+  should "not save a note without a need_id" do
+   note = Note.new(@atts.except(:need_id))
+
+   refute note.save
+   assert note.errors.has_key?(:need_id)
+  end
+
+  should "not save a note without a valid need_id" do
+   note = Note.new(@atts.merge(need_id: :foo))
+
+   refute note.save
+   assert note.errors.has_key?(:need_id)
+  end
+
+  should "not save a note without text" do
+   note = Note.new(@atts.except(:text))
+
+   refute note.save
+   assert note.errors.has_key?(:text)
+  end
+
+  should "not save a note without an author" do
+   note = Note.new(@atts.except(:author))
+
+   refute note.save
+   assert note.errors.has_key?(:author)
   end
 end
