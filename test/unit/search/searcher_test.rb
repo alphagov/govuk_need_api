0344e56120b847c83a6e9aa278a94077a9d32458
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2014-03-20 14:57:43 +0000

    Request 50 results per search.
    By default, elasticsearch returns 10 results, which isn't enough.
    Returning 50 is consistent with the list pagination.

--- test/unit/search/searcher_test.rb
+++ test/unit/search/searcher_test.rb
@@ -64,6 +64,16 @@ class SearcherTest < ActiveSupport::TestCase
     Search::Searcher.new(client, "foo", "bang").search("baz")
   end
 
+  should "ask for 50 results" do
+    client = mock("client")
+    client.expects(:search).with { |search_params|
+      query_params = search_params[:body]["query"].values.first
+      query_params["query"] == "baz"
+    }.returns(single_result_response)
+
+    Search::Searcher.new(client, "foo", "bang").search("baz")
+  end
+
   should "parse out the search results" do
     client = mock("client")
     client.expects(:search).returns(single_result_response)
