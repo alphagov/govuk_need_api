3dd0864bf32753973735664eff4d51c2e1912874
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-10-09 15:09:29 +0000

    Update organisations once per hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
 job_type :rake, 'cd :path && /usr/local/bin/govuk_setenv need-api bundle exec rake :task :output'
 job_type :run_script, 'cd :path && RAILS_ENV=:environment /usr/local/bin/govuk_setenv need-api script/:task :output'
 
-every 1.day, :at => '4:00am' do
+every :hour do
   rake "organisations:import"
 end
