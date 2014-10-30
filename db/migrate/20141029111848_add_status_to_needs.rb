84b37bd344abd766b9f4450fb8b716db2ca4a120
Jake Benilov <benilov@gmail.com>
2014-10-30 11:01:31 +0000

    Migration to add status to all existing needs

---
+++ db/migrate/20141029111848_add_status_to_needs.rb
@@ -0,0 +1,16 @@
+class AddStatusToNeeds < Mongoid::Migration
+  def self.up
+    Need.all.each do |n|
+      unless n.status.present?
+        if n.in_scope.nil?
+          n.status = NeedStatus.new(description: "proposed")
+        elsif n.in_scope == false
+          n.status = NeedStatus.new(description: "out of scope", reason: n.out_of_scope_reason)
+        else
+          raise "Unexpected 'in_scope' value: #{n.in_scope.inspect} for #{n.inspect}"
+        end
+        n.save_as(name: "Need Importer")
+      end
+    end
+  end
+end
