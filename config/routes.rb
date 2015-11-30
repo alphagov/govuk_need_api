b437559f7bbba728a8d4744484e169d5aaa43f8a
Ben Lovell <benjamin.lovell@gmail.com>
2015-11-30 16:04:44 +0000

    Autocorrect violations in `config` directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :organisations, :only => :index
-  resources :needs, :except => [:new, :edit]
-  resources :notes, :only => [:create]
+  resources :organisations, only: :index
+  resources :needs, except: [:new, :edit]
+  resources :notes, only: [:create]
 
   put '/needs/:id/closed', to: 'needs#closed'
   delete '/needs/:id/closed', to: 'needs#reopen'
 
   get "/healthcheck" => Proc.new { [200, {"Content-type" => "text/plain"}, ["OK"]] }
 
-  root :to => "root#index"
+  root to: "root#index"
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
+set :output, { error: 'log/cron.error.log', standard: 'log/cron.log' }
 job_type :rake, 'cd :path && /usr/local/bin/govuk_setenv need-api bundle exec rake :task :output'
 job_type :run_script, 'cd :path && RAILS_ENV=:environment /usr/local/bin/govuk_setenv need-api script/:task :output'
 
