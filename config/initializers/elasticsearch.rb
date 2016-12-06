e6221eb6bf91edb5a002f6da270c2415051d53b8
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-12-06 11:13:28 +0000

    Add sensible default for Elasticsearch hosts
    Just so that this doesn't have to be set by most people locally.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 # NOTE: the `search.rb` initializer relies on this file being loaded first,
 # which it currently manages because it comes first in the alphabet.
 GovukNeedApi.search_client = Elasticsearch::Client.new(
-  hosts: ENV['ELASTICSEARCH_HOSTS'].split(',')
+  hosts: (ENV['ELASTICSEARCH_HOSTS'] || "http://localhost:9200").split(',')
 )
