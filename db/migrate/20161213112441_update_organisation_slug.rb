21966e3c322641a79ff9f1e8a717fad862fdd779
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-12-13 11:45:36 +0000

    Update organisation slug
    The content-id of the organisation could not be found as its slug has changed,
    which means it was impossible to add the organisation as a link to the needs
    that link to it in the Publishing-API.

---
+++ db/migrate/20161213112441_update_organisation_slug.rb
@@ -0,0 +1,31 @@
+class UpdateOrganisationSlug < Mongoid::Migration
+  def self.up
+    organisation = Organisation.find_by(slug: "government-actuary-s-department")
+    organisation.update_attributes({
+      _id: "government-actuarys-department",
+      slug: "government-actuarys-department"
+    })
+    needs_to_correct = Need.where(organisation_ids: ["government-actuary-s-department"])
+    needs_to_correct.each do |n|
+      n.organisation_ids=["government-actuarys-department"]
+      n.save
+    end
+  end
+
+  def self.down
+    organisation =
+      Organisation.find_by(slug: "government-actuary-s-department") ||
+      Organisation.find_by(slug: "government-actuarys-department")
+    organisation.update_attributes({
+      _id: "government-actuary-s-department",
+      slug: "government-actuary-s-department"
+    })
+    needs_to_correct =
+    Need.where( organisation_ids: ["government-actuary-s-department"] ) ||
+    Need.where( organisation_ids: ["government-actuarys-department"] )
+    needs_to_correct.each do |n|
+      n.organisation_ids=["government-actuarys-department"]
+      n.save
+    end
+  end
+end
